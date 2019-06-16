793eab5ebb9475243dde7d31e2898700226be7fd
Philip Schulz Klingauf <philip@schulz-klingauf.de>
2019-06-16 02:27:02 +0200

    formatting

--- wp-extractblogposts.py
+++ wp-extractblogposts.py
@@ -15,34 +15,34 @@ PYTHON_ENCODING = 'utf-8'
 COUNTER = 0
 
 # Shell parameter: run mode -> dev mode (reset all database!)
-if len(sys.argv) > 1:
-    BLOG_URL = sys.argv[1]
+if len( sys.argv ) > 1:
+    BLOG_URL = sys.argv[ 1 ]
 else:
-    raise ValueError( 'Missing parameter: WorPress blog domain. E. g. "https://myblog.com"' )
+    raise ValueError( 'Missing parameter: WordPress blog domain. E. g. "https://myblog.com"' )
 
 
 ############ METHODS
 
-def httpGetJson(url):
+def httpGetJson( url ):
     response_body = urllib2.urlopen( url ).read()
     response_dictionary = json.loads( response_body )
     return response_dictionary
 
 def getPosts():
-    return httpGetJson(BLOG_URL + '/wp-json/wp/v2/posts?per_page=100')
+    return httpGetJson( BLOG_URL + '/wp-json/wp/v2/posts?per_page=100' )
 
-def getFeaturedImagesLinkResolved(url):
+def getFeaturedImagesLinkResolved( url ):
     mediaData = httpGetJson(url)
     return mediaData['source_url']
 
-def getPostContents(response):
+def getPostContents( response ):
     # get featured images links and blog contents
     for val in response:
         global COUNTER
 
         try:
-            blogTitle = val['title']['rendered'].encode( PYTHON_ENCODING )
-            blogContent = val['content']['rendered'].encode( PYTHON_ENCODING )
+            blogTitle = val[ 'title' ][ 'rendered' ].encode( PYTHON_ENCODING )
+            blogContent = val[ 'content' ][ 'rendered' ].encode( PYTHON_ENCODING )
 
             currentDir = OUTPUT_DIR + '/' + blogTitle
             os.mkdir( currentDir )
@@ -53,7 +53,7 @@ def getPostContents(response):
 
             # download and save blogpost's featured image if exist
             try:
-                featuredImageUrl = val['_links']['wp:featuredmedia'][0]['href']
+                featuredImageUrl = val[ '_links' ][ 'wp: featuredmedia'][ 0 ][ 'href' ]
                 featuredImagesLinkResolved = getFeaturedImagesLinkResolved( featuredImageUrl )
                 urllib.urlretrieve( featuredImagesLinkResolved, currentDir + '/' + blogTitle + '.jpg' )
             except:
@@ -68,7 +68,7 @@ def getPostContents(response):
 def run():
     print '### WP EXTRACT BLOG POSTS: running...'
     getPostContents( getPosts() )
-    print '### WP EXTRACT BLOG POSTS: Successfully extracted ' + str(COUNTER) + ' blogposts from ' + BLOG_URL
+    print '### WP EXTRACT BLOG POSTS: Successfully extracted ' + str( COUNTER ) + ' blogposts from ' + BLOG_URL
 
 ############ EXECUTE
 
